d1e3f0f9a0d5d99df52cba8d1290198bbfe7fcd8
Kamille <kamille.kpimentel@gmail.com>
2024-10-22 20:02:10 -0300

    config inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Example from "./pages/Example";
 
 function App() {
   return (
     <div className="App">
-      <Example />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Example />} />
+          <Route path="/teste" element={<Example />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
