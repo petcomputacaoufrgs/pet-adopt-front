d5ede384583cc6f252031fb8ef730ab91968d27d
Kamille Konarzewski <kamille.kpimentel@gmail.com>
2025-04-11 14:21:23 -0300

    footer quase completo (falta botão link)

--- src/views/HomePage/6Footer/index.tsx
+++ src/views/HomePage/6Footer/index.tsx
@@ -1,81 +1,112 @@
 import {
-    GridContainer,
-    FooterGrid,
-    FooterDiv,
-    FooterLinks,
-    FooterSection,
-    SocialIconsDiv,
-    Image3,
-    Rights,
-    Image
-  } from "./styles";
-    
-  import Logo from "../../../assets/HorizontalLogo.png";
-  import Insta from "../../../assets/InstaPin.png";
-  import Facebook from "../../../assets/FacebookPin.png";
-  import Youtube from "../../../assets/YoutubePin.png";
-  import Tiktok from "../../../assets/TiktokPin.png";
+  GridContainer,
+  FooterGrid,
+  FooterDiv,
+  FooterLinks,
+  FooterSection,
+  SocialIconsDiv,
+  Image3,
+  Rights,
+  Icon} from "./styles";
+
+import Logo from "../../../assets/HorizontalLogo.png";
+import Insta from "../../../assets/OrangeInstagramPin.png";
+import Facebook from "../../../assets/OrangeFacebookPin.png";
+import Youtube from "../../../assets/OrangeYoutubePin.png";
+import Tiktok from "../../../assets/OrangeTiktokPin.png";
+
+import InstaB from "../../../assets/BrownInstagramPin.png";
+import FacebookB from "../../../assets/BrownFacebookPin.png";
+import YoutubeB from "../../../assets/BrownYoutubePin.png";
+import TiktokB from "../../../assets/BrownTiktokPin.png";
+
+const Footer = () => {
+  const links1 = [
+    { label: "Institucional", href: "#" },
+    { label: "Sobre Nós", href: "#" },
+    { label: "Animais Recém Adicionados", href: "#" },
+    { label: "Dicas", href: "#" },
+    { label: "Fale Conosco", href: "#" },
+    { label: "Cadastrar ONG", href: "#" },
+    { label: "Política de Privacidade", href: "#" }
+  ];
+
+  const links2 = [
+    { label: "Login", href: "#" },
+    { label: "Sou Administrador", href: "#" },
+    { label: "Sou uma ONG", href: "#" },
+    { label: "Sou Membro de ONG", href: "#" }
+  ];
+
+  const socialMediaLinks = [
+    {
+      orange: Insta,
+      brown: InstaB,
+      alt: "Instagram",
+      href: "https://www.instagram.com"
+    },
+    {
+      orange: Facebook,
+      brown: FacebookB,
+      alt: "Facebook",
+      href: "https://www.facebook.com"
+    },
+    {
+      orange: Youtube,
+      brown: YoutubeB,
+      alt: "YouTube",
+      href: "https://www.youtube.com"
+    },
+    {
+      orange: Tiktok,
+      brown: TiktokB,
+      alt: "TikTok",
+      href: "https://www.tiktok.com"
+    }
+  ];
   
-  const Footer = () => {
-    const links1 = [
-      "Institucional",
-      "Sobre Nós",
-      "Animais Recém Adicionados",
-      "Dicas",
-      "Fale Conosco",
-      "Cadastrar ONG",
-      "Política de Privacidade"
-    ];
-  
-    const links2 = [
-      "Login",
-      "Sou Administrador",
-      "Sou uma ONG",
-      "Sou Membro de ONG"
-    ];
-  
-    return (
-      <GridContainer>
-        <FooterGrid>
-          <FooterDiv>
-            <Image3 src={Logo} />
-            <p>
-              Nosso objetivo é <strong>Alcançar Finais Felizes</strong>, proporcionando lares amorosos e seguros para todos os animais.
-            </p>
-          </FooterDiv>
-  
-          <FooterLinks>
-            <FooterSection>
-              {links1.map((text, index) => (
-                <p key={index}>{text}</p>
-              ))}
-            </FooterSection>
-    
-            <FooterSection>
-              {links2.map((text, index) => (
-                <p key={index}>{text}</p>
-              ))}
-            </FooterSection>
-    
-            <FooterSection>
-              <p>Mídias Sociais</p>
-              <SocialIconsDiv>
-                <Image src={Insta}/>
-                <Image src={Facebook}/>
-                <Image src={Youtube}/>
-                <Image src={Tiktok}/>
-              </SocialIconsDiv>
-            </FooterSection>
-          </FooterLinks>
-        </FooterGrid>
-            
-        <Rights>
-          <p>© 2024 Pet Adopt. Todos os direitos reservados.</p>
-          <p>Desenvolvido por: Pet Computação</p>
-        </Rights>
-      </GridContainer>
-    );
-  };
-  
-  export default Footer;
-  
+  return (
+    <GridContainer>
+      <FooterGrid>
+        <FooterDiv>
+          <Image3 src={Logo} />
+          <p>
+            Nosso objetivo é <strong>Alcançar Finais Felizes</strong>, proporcionando lares amorosos e seguros para todos os animais.
+          </p>
+        </FooterDiv>
+
+        <FooterLinks>
+          <FooterSection>
+            {links1.map((link, index) => (
+              <a key={index} href={link.href}><p>{link.label}</p></a>
+            ))}
+          </FooterSection>
+
+          <FooterSection>
+            {links2.map((link, index) => (
+              <a key={index} href={link.href}><p>{link.label}</p></a>
+            ))}
+          </FooterSection>
+
+          <FooterSection>
+            <p>Mídias Sociais</p>
+            <SocialIconsDiv>
+            {socialMediaLinks.map((icon, index) => (
+              <a key={index} href={icon.href} target="_blank" rel="noopener noreferrer">
+                <Icon $orange={icon.orange} $brown={icon.brown} aria-label={icon.alt} />
+              </a>
+            ))}
+            </SocialIconsDiv>
+          </FooterSection>
+        </FooterLinks>
+      </FooterGrid>
+
+      <Rights>
+        <p>© 2024 Pet Adopt. Todos os direitos reservados.</p>
+        <p>Desenvolvido por: Pet Computação</p>
+      </Rights>
+    </GridContainer>
+  );
+};
+
+export default Footer;
--- src/views/HomePage/6Footer/styles.ts
+++ src/views/HomePage/6Footer/styles.ts
@@ -11,68 +11,103 @@ export const GridContainer = styled.div`
   justify-content: center;
   align-items: center;
 
+  font-size: 18px;
+
   p {
     margin: 0;
+  }
+
+  @media (max-width: 915px) {
+    padding: 64px 28px 24px 28px;
   }
 `;
 
 export const FooterGrid = styled.div`
-  margin: 95px 200px 65px 200px;
-  width: 1435px;
+  margin: 5% 10% 3.3% 10%;
+  width: 75%;
 
-  font-size: 18px;
+  font-size: 1em;
 
   display: flex;
   justify-content: space-around;
+
+  @media (max-width: 1200px) {
+    font-size: 0.88em;
+  }
+
+  @media (max-width: 915px) {
+    width: 86%;
+    margin: 0;
+
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 export const FooterDiv = styled.div`
-  width: 255px;
-  margin-right: 255px;
+  width: 18%;
+  margin-right: 18%;
 
   font-family: 'Nunito Sans';
 
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  @media (max-width: 915px) {
+    margin-right: 0;
+    margin-bottom: 10%;
+    width: 100%;
+
+    p,strong{
+      max-width: 370px;
+      text-align: center;
+    }
+  }
 `;
 
 export const Image3 = styled.img`
-  width: 257px;
-  height: 105px;
-  margin-bottom: 45px;
+  width: 100%;
+  height: auto;
+  margin-bottom: 18%;
+
+  @media (max-width: 915px) {
+    width: 45%;
+    height: auto;
+    max-width: 200px;
+    margin-bottom: 8%;
+  }
 `;
 
 export const FooterLinks = styled.div`
-  width: 920px;
+  width: 64%;
+  gap: 9%;
 
   font-family: 'Source Serif 4';
   font-weight: 800;
-  font-size: 18px;
 
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
+  justify-content: space-between;
 
+  @media (max-width: 915px) {
+    width: 100%;
+    margin-bottom: 10%;
+
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 export const FooterSection = styled.div`
-  gap: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-start;
-
-  p {
-    color: #553525;
-    transition: color 0.1s ease;
-
-    font-family: 'Source Serif 4', serif;
-    font-weight: 800;
-    font-size: clamp(0.8rem, 1vw, 0.7rem);
+  
+a {
+    margin-bottom: 16px;
     line-height: 2;
-
-    margin: 0;
+    text-decoration: none;
+    color: #553525;
 
     &:hover {
       color: #8b6a50;
@@ -80,50 +115,75 @@ export const FooterSection = styled.div`
     }
   }
 
-  @media (max-width: 1200px) {
+  p{
+    margin-bottom: 16px;
+    line-height: 2;
+  }
+
+  @media (max-width: 915px) {
+    margin-bottom: 10%;    
+
+    display: flex;
+    flex-direction: column;
     align-items: center;
-    width: 100%;
-    gap: 10px;
   }
 `;
 
 export const SocialIconsDiv = styled.div`
-  gap: 15px;
+  gap: 24px;
   width: 100%;
 
   display: flex;
+  flex-direction: row;
 
-  @media (max-width: 1200px) {
-    gap: 10px;
-    justify-content: center;
+  a{
+    width: 13%;
+    height: auto;
+  }
+
+  img{
+  
   }
 `;
 
-export const Image = styled.img`
+export const Icon = styled.div<{ $orange: string; $brown: string }>`
   width: 40px;
   height: 40px;
+  background-image: url(${props => props.$orange});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  transition: background-image 0.3s ease-in-out;
+
+  &:hover {
+    background-image: url(${props => props.$brown});
+  }
 `;
 
 export const Rights = styled.div`
+  width: 79%;
   border-top: 1px solid #D9D2CE;
-  width: 1515px;
-  margin-bottom: 15px;
+  margin-bottom: 0.8%;
 
   font-weight: 400;
-  font-size: 14px;
+  font-size: 0.77em;
   font-family: 'Nunito Sans';
 
   display: flex;
   justify-content: space-between;
 
   p {
-    margin: 0;
     line-height: 3;
   }
 
-  @media (max-width: 1200px) {
+  @media (max-width: 915px) {
+    width: 86%%;
+    margin-bottom: 2%;
+   
+    font-size: 0.66em;
+  
+    display: flex;
     flex-direction: column;
     align-items: center;
-    margin-bottom: 20px;
   }
 `;
