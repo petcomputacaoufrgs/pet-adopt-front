227f9f4389e25f45bfe46b73a895ccfd36edaef9
Eduardo Fonseca da Silva <edufon1503@gmail.com>
2025-04-22 20:23:11 -0300

    Retirando testes

--- src/views/HomePage/index.tsx
+++ src/views/HomePage/index.tsx
@@ -8,35 +8,12 @@ import ListAnimals from "./3ListAnimals";
 import Dicas from "./4Dicas";
 import Contact from "./5Contact";
 import Footer from "./6Footer";
+import RadioButton from "../../components/RadioButton";
 
 const HomeView = () => {
 
-  const options = ["A", "B", "C"];
-
-  const handleSelectButton = (value : string) => {
-    switch(value){
-      case "A":
-        console.log("A");
-        break;
-      case "B":
-        console.log("B");
-        break;
-      case "C":
-        console.log("C");
-        break;
-    }
-  }
-
-  const indicator = (selected : boolean) => {
-    return selected? <ChevronDown /> : <ChevronUp />;
-  }
-
-
   return (
     <>
-      <DropdownButton label="Teste" options={options} onClick={handleSelectButton} indicator={indicator}  />
-      <SearchBar title="Países" required={true} placeholder="Encontre o país" width="500px" fontSize="20px" options={["Brasil", "Burkina Faso", "EUA", "China", "Argentina", "Bolívia", "Brasil mas a segunda versão"]} />
-      <p>TESTE</p>
       <Banner />
       <Actions />
       <About></About>
