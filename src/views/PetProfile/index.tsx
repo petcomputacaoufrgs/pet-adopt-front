f7ec3ca56ac26be358b36514f6df5c4675f6f870
Eduardo Veiga <eduardo85011162@gmail.com>
2025-07-30 20:40:44 -0300

    PetProfile: adicionado Cards de Pet e ONG

--- src/views/PetProfile/index.tsx
+++ src/views/PetProfile/index.tsx
@@ -3,20 +3,103 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 import {
-    BackButtonContainer,
     Container,
-    Main
+    Main,
+    BackButtonContainer,
+    InfoCard,
+    CardHeader,
+    Title,
+    TagsContainer,
+    InfoContainer,
+    InfoElement,
+    CardAbout,
+    Cards,
+    Icon,
+    SocialIconsDiv,
 } from "./styles";
+
+import { ChevronLeft, Info } from 'lucide-react';
+import { CircleCheck } from 'lucide-react';
+import { PawPrint } from 'lucide-react';
+import { MapPin } from 'lucide-react';
+import { Mail } from 'lucide-react';
+import { Phone } from 'lucide-react';
+
 
 import Header from "../../components/Header";
 import Footer from "../HomePage/6Footer";
 import ActionText from "../../components/ActionText";
-import { ChevronLeft } from 'lucide-react';
-
+import Tag from "../../components/Tags";
 
 import loginPageLogo from "../../assets/HorizontalLogo.png";
+import DogForCard from "../../assets/HomePageCardDog.png";
+import Insta from "../../assets/OrangeInstagramPin.png";
+import Facebook from "../../assets/OrangeFacebookPin.png";
+import Youtube from "../../assets/OrangeYoutubePin.png";
+import Tiktok from "../../assets/OrangeTiktokPin.png";
+import InstaB from "../../assets/BrownInstagramPin.png";
+import FacebookB from "../../assets/BrownFacebookPin.png";
+import YoutubeB from "../../assets/BrownYoutubePin.png";
+import TiktokB from "../../assets/BrownTiktokPin.png";
+
+/* Dados temporários dos animais a serem exibidos na tela.
+* Idealmente, no futuro devem ser carregados do backend conforme a página atual.
+*/
+const pet = { 
+    image_url: DogForCard, 
+    flag: true, 
+    specie: "Cachorro", 
+    sex: "Fêmea", 
+    size: "Porte Médio", 
+    name: "Mel", 
+    race: "Vira-lata", 
+    age: "2", 
+    location: "São Paulo, SP", 
+    description: "Mel é uma cachorrinha muito carinhosa e brincalhona. Ela adora correr e brincar com outros animais. Está pronta para encontrar um lar cheio de amor e carinho."
+};
+
+const ong = {
+    name: "ONG Adoção Feliz",
+    location: "São Paulo, SP",
+    contact: "(11) 1234-5678",
+    email: "contato@amorpetong.org",
+    description: "O Cão Sem Dono é uma ONG (Organização Não Governamental), sem fins lucrativos, e que nasceu de um grande sonho do seu atual presidente: tirar o maior número possível de animais das ruas, dar tratamento adequado e integrá-los a famílias que lhes deem amor, carinho e uma vida digna. /n/nFoi criada informalmente em 2005 na cidade de São Paulo. Seu estatuto foi lançado e registrado em 23 de Abril de 2008, mesmo dia em que obteve seu CNPJ./n/nAtualmente a ONG mantém 2 abrigos (a sede fica em Itapecerica da Serra, SP) com 450 animais que são constantemente tratados por veterinários, alimentados com ração de boa qualidade, bebem água potável, dormem em abrigos especialmente construídos e são tratados com muito amor e carinho por todos os funcionários e voluntários que estão sempre visitando as instalações onde ficam os cães./n/nSua equipe é formada por Presidente, Diretores e 33 colaboradores entre tratadores, veterinários, auxiliares de veterinária, equipe do Bazar, equipe de resgate e a equipe do escritório.",
+    social_media: {
+        facebook: "https://www.facebook.com",
+        instagram: "https://www.instagram.com",
+        twitter: "https://twitter.com",
+    }
+}
+
 
 const PetProfile: React.FC = () => {
+
+    const socialMediaLinks = [
+        {
+        orange: Insta,
+        brown: InstaB,
+        alt: "Instagram",
+        href: "https://www.instagram.com"
+        },
+        {
+        orange: Facebook,
+        brown: FacebookB,
+        alt: "Facebook",
+        href: "https://www.facebook.com"
+        },
+        {
+        orange: Youtube,
+        brown: YoutubeB,
+        alt: "YouTube",
+        href: "https://www.youtube.com"
+        },
+        {
+        orange: Tiktok,
+        brown: TiktokB,
+        alt: "TikTok",
+        href: "https://www.tiktok.com"
+        }
+    ];
     
     const headerOptions = [
     "Sobre Nós",
@@ -38,6 +121,7 @@ const PetProfile: React.FC = () => {
     const currentUserOptions = ["Manage Infors"];
 
     const currentUserActions = handleUserAction;
+
     return (
         <Container>
 
@@ -63,6 +147,120 @@ const PetProfile: React.FC = () => {
 
                     </ActionText>
                 </BackButtonContainer>
+
+                <Cards>
+
+                    <InfoCard>
+
+                        <CardHeader>
+                            
+                            <Title>
+                                <h1> {pet.name}</h1>
+
+                                <TagsContainer>
+
+                                    <Tag $text={pet.specie} type={"light"} fontSize={"14px"} />
+
+                                    <Tag $text={pet.sex} type={"light"} fontSize={"14px"} />
+
+                                    <Tag $text={pet.size} type={"light"} fontSize={"14px"} />
+                                    <Tag $text={pet.race} type={"light"} fontSize={"14px"} />
+
+                                </TagsContainer>
+                            </Title>
+
+                            <InfoContainer>
+
+                                <InfoElement>
+                                    <CircleCheck size={20} color= "#FFFFFF" fill="#FF9944"/>
+                                    <h4> Disponivel </h4>
+                                </InfoElement>
+
+                                <InfoElement>
+                                    <PawPrint size={20} color="#FF9944" fill = "#FF9944"/>
+                                    <h4> {pet.age} Anos de Idade </h4>
+                                </InfoElement>
+
+                                <InfoElement>
+                                    <PawPrint size={20} color="#FF9944" fill = "#FF9944"/>
+                                    <h4> {pet.location} </h4>
+                                </InfoElement>
+
+                            </InfoContainer>
+
+                        </CardHeader>
+
+                        <CardAbout>
+                            <h2> Sobre o Pet </h2>
+                            <h3> {pet.description} </h3>
+                        </CardAbout>
+
+                    </InfoCard>
+
+                    <InfoCard>
+
+                        <CardHeader>
+                            <Title>
+                                <h2> {ong.name} </h2>
+                            </Title>
+
+                            <InfoContainer>
+                                
+                                <InfoElement>
+                                    <MapPin size={20} color="#FFFFFF" fill = "#FF9944"/>
+                                    <h4> {ong.location} </h4>
+                                </InfoElement>
+
+                                <InfoElement>
+                                    <Mail size={20} color="#FFFFFF" fill = "#FF9944"/>
+                                    <h4> {ong.email} </h4>
+                                </InfoElement>
+
+                                <InfoElement>
+                                    <Phone size={20} color="#FFFFFF" fill = "#FF9944"/>
+                                    <h4> {ong.contact} </h4>
+                                </InfoElement>
+
+                            </InfoContainer>
+
+                            <h3> 
+                                
+                                {ong.description.length > 280
+                                    ? ong.description.slice(0, 280) + "..."
+                                    : ong.description
+                                } 
+
+                                <span
+                                    style={{
+                                    color: "#563526",
+                                    fontWeight: 700,
+                                    cursor: "pointer",
+                                    marginLeft: 4,
+                                    textDecoration: "underline"
+                                    }}
+                                    onClick={() => navigate("/validateNgoProfile")}
+                                >
+                                    Saber Mais
+                                </span>
+                                
+                            </h3>
+                                
+                        </CardHeader>  
+                            
+                        <SocialIconsDiv>
+
+                            <h3> Acompanhe a ONG nas Redes Sociais: </h3>
+
+                            {socialMediaLinks.map((icon, index) => (
+                                <a key={index} href={icon.href} target="_blank" rel="noopener noreferrer">
+                                <Icon $orange={icon.orange} $brown={icon.brown} aria-label={icon.alt} />
+                                </a>
+                            ))}
+                        </SocialIconsDiv>
+
+                    </InfoCard>
+
+                </Cards>
 
             </Main>
 
--- src/views/PetProfile/styles.ts
+++ src/views/PetProfile/styles.ts
@@ -10,7 +10,6 @@ export const Container = styled.div`
     color: #563526;
     
 `
-
 export const Main = styled.main`
     
     width: 100%;
@@ -32,16 +31,36 @@ export const Main = styled.main`
     h1{
         font-size: 2rem; // 32px em rem utilizando 16px como base
         font-weight: 700;
+        margin: 0;
     }
 
     h2{
         font-size: 1.125rem; // 18px em rem utilizando 16px como base
         font-weight: 700;
+        margin: 0;
     }
     
     h3{
         font-size: 1rem; // 16px em rem utilizando 16px como base
+        font-weight: 400;
+        margin: 0;
     }
+
+    h4{
+        font-size: 1rem; // 16px em rem utilizando 16px como base
+        font-weight: 400;
+        margin: 0;
+        color: #755B4D;
+    }
+        
+`
+
+export const Cards = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+
+    gap: 16px;
 `
 
 export const BackButtonContainer = styled.div`
@@ -49,6 +68,94 @@ export const BackButtonContainer = styled.div`
     justify-content: flex-start;
     align-items: flex-start;
     width: 100%;
-
-
+    padding: 53px 0;
+    box-sizing: border-box;
 `
+
+export const InfoCard = styled.div`
+    width: 38.9583333vw; // 750px em vw utilizando 1920px como base
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    padding: 2.5vw; // 40px em vw utilizando 1920px como base
+
+    gap: 32px;
+
+    box-sizing: border-box;
+
+    border-radius: 20px;
+    border: 0.1px solid #DEDEDE;
+`
+export const Title = styled.div
+`
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+`
+export const CardHeader = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+`
+export const TagsContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 12px;
+`
+
+export const InfoContainer = styled.div`
+    
+    width: 100%;
+
+    display: flex;
+    flex-direction: row;
+    
+    padding-top: 16px;
+    padding-bottom: 16px;
+    gap: 24px;
+
+    border-top: 0.1px solid #DEDEDE;
+    border-bottom: 0.1px solid #DEDEDE;
+
+    box-sizing: border-box;
+`
+
+export const InfoElement = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 12px;
+`
+
+export const CardAbout = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    gap: 12px;
+`
+export const Icon = styled.div<{ $orange: string; $brown: string }>`
+  width: 35px;
+  height: 35px;
+  background-image: url(${props => props.$orange});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  transition: background-image 0.3s ease-in-out;
+
+  &:hover {
+    background-image: url(${props => props.$brown});
+  }
+`;
+
+export const SocialIconsDiv = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: row;  
+
+  align-items: center;
+
+  gap: 16px
+
+`;
